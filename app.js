3315ea4f8a5f7a8901e3c9172ec84cc5701b0dc1
msallito510 <msallito510@gmail.com>
2020-05-03 00:23:56 +0200

    copy webdev1219/starter-backend-m3, port backend 3001

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv").config();
 
-var app = express();
+const dbPath = process.env.MONGODB_URI;
 
-app.use(logger('dev'));
+mongoose
+  .connect(dbPath, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`conected to ${dbPath}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const authRouter = require("./routes/auth");
+const demoRouter = require("./routes/demo");
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_DOMAIN],
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    name: "ironhack",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", authRouter);
+app.use("/protected", demoRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ code: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error("ERROR", req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ code: "unexpected", error: err });
+  }
+});
 
 module.exports = app;
